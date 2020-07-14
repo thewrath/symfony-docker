d8d55e4e2b1a4fac5f23b1f0cd73085faba5b73d
thewrath <epis.ajout@gmail.com>
2020-07-14 13:37:38 +0200

    Fix - volumes mount

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,5 @@
 set -e
 
 # Up this Symfony project
-bash -c "composer install"
-bash -c "symfony server:ca:install"
-bash -c "symfony server:stop"
-bash -c "symfony server:start --port=80 --no-tls"
+bash -c "cd ./var/www/app && composer install"
+bash -c "cd ./var/www/app && php -S localhost:80"
